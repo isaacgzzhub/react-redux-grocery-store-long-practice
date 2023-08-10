f6748e59d05c0374dd662b37179bf1513956d435
Isaac Gzz <isaacgzz.ing@gmail.com>
2023-08-09 22:01:24 -0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Cart from './components/Cart';
 import ProduceList from './components/ProduceList';
+import { useDispatch } from 'react-redux';
+import { populateProduce } from './store/produce';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(populateProduce());
+  }, []);
   const [showCart, setShowCart] = useState(false);
-  
+
   return (
     <>
       <nav>
--- src/store/produce.js
+++ src/store/produce.js
@@ -1,18 +1,25 @@
 // don't use as keyword, just name it in the import
 import produceData from ".././mockData/produce.json";
 
-export default function produceReducer(state = {}, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
-
 const POPULATE = "produce/POPULATE";
 
 export const populateProduce = () => {
   // need to wrap in return statement
   return {
     type: POPULATE,
+    produce: produceData
   };
-};
+}
+
+export default function produceReducer(state = {}, action) {
+  switch (action.type) {
+    case POPULATE:
+      const newState = {};
+      action.produce.forEach(produce => {
+        newState[produce.id] = produce;
+      });
+      return newState;
+    default:
+      return state;
+  }
+}
